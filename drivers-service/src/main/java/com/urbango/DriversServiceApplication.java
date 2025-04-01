120b0d7d16fee774b1f39d16c70e146985fb38fc
UrbanGo651 <urbango280325@gmail.com>
2025-04-01 11:07:41 -0500

    Estrucutra del backend

---
+++ drivers-service/src/main/java/com/urbango/DriversServiceApplication.java
@@ -0,0 +1,11 @@
+package com.urbango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriversServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DriversServiceApplication.class, args);
+    }
+}
